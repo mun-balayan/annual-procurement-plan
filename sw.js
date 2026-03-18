31916c3317d71c20fb3f9454956b696253583308
Cholskieee <152066185+Cholskiee@users.noreply.github.com>
2026-03-18 16:41:06 +0800

    minor fixes

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // APP-CSE Service Worker — network-first
-const CACHE = 'app-cse-v12';
+const CACHE = 'app-cse-v13';
 const ASSETS = ['./', './index.html'];
 
 self.addEventListener('install', e => {
