94ccdce2d6d5fdd7653750041bbf77ab92466be1
Cholskieee <152066185+Cholskiee@users.noreply.github.com>
2026-03-31 10:48:18 +0800

    printing adjustments

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // APP-CSE Service Worker — network-first
-const CACHE = 'app-cse-v18';
+const CACHE = 'app-cse-v19';
 const ASSETS = ['./', './index.html'];
 
 self.addEventListener('install', e => {
