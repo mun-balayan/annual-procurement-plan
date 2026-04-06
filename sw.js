d42f405755dc13de70651b039bee449af7d15abd
Cholskieee <152066185+Cholskiee@users.noreply.github.com>
2026-04-06 08:34:09 +0800

    Remove the remove button and remove the built in instead change the name of the department or add

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // APP-CSE Service Worker — network-first
-const CACHE = 'app-cse-v20';
+const CACHE = 'app-cse-v22';
 const ASSETS = ['./', './index.html'];
 
 self.addEventListener('install', e => {
